191821c315dae62e0af4a3d7d932d65cb64578d1
SantaGosu <taz.c.munoz@gmail.com>
2022-10-11 22:59:17 +0000

    made a api route for token, installed jwt and added jwt config to routes.py and app.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,23 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+import os
+
+
 api = Blueprint('api', __name__)
 
+@api.route("/token", methods=["POST"])
+def create_token():
+    username = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
